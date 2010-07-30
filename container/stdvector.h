0bc1d73919f3607bfcf1593b9dddc79406ecdc79
Anton Petersson <anton-vcs@anp.nu>
2010-07-30 13:51:19 +0200

    Add iwritablecontainer and std::vector based impl.

---
+++ container/stdvector.h
@@ -0,0 +1,54 @@
+/*
+
+Copyright (c) 2010, Anton Petersson < anton (at) anp.nu >
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef _STDVECTOR_H_
+#define _STDVECTOR_H_
+
+#include "iwritablecontainer.h"
+
+namespace anp
+{
+
+/**
+ * Simple wrapper around std::vector that can be passed to 
+ * functions expecting IWritableContainer interfaces.
+ * Provides no encapsulation or real wrapping around the
+ * std::vector object.
+ */
+template<typename T>
+class StdVector: public anp::IWritableContainer<T>
+{
+public:
+	void pushBack(T &element)
+	{
+		m_vector.push_back(element);
+	}
+	
+	std::vector<T> m_vector;
+} // namespace anp
+
+#endif // _HEADER_NAME_
